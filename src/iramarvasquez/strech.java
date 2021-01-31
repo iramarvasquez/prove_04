e58c07347d56ae66fef4da7ef4cb9b9d50e9799e
iramarvasquez <iramarvasquez@gmail.com>
2021-01-30 22:10:23 -0300

    Stretch Challenge Part

---
+++ src/iramarvasquez/strech.java
@@ -0,0 +1,8 @@
+package iramarvasquez;
+
+public class strech {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Testing the Stretch Challenge Part");
+    }
+}
